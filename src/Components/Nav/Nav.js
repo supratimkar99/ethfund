53178b7602ad4cc35114ac09e3e624fa14e88bec
Syed Inayath <syedinayath1999@gmail.com>
2021-05-22 23:12:59 +0530

    commit2

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,26 +5,18 @@ function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light static-top header-a">
             <div className="container nav-container">
-                {/* <a className="navbar-brand brand" href="#">EthFund</a> */}
                 <span className="navbar-brand">
                         <img src={ethpic} width="45" height="45" />
                     </span>
-                <h1>EthFund</h1>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse alink" id="navbarSupportedContent">
+                <h1>EthFund</h1> 
+                 <div className="collapse navbar-collapse alink" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">About Us <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
+                        <li className="nav-itm active">
                             <a className="nav-link" href="#">Contact Us</a>
                         </li>
-
-                        <a className="btn btn-outline-dark start" href="#">Login</a>
-                        <a className="btn btn-outline-dark start" href="#">Sign Up</a>
+                        <a className="btn btn-outline-dark start" id="btn" href="#">Login</a>
+                        <a className="btn btn-outline-dark start" id="btn" href="#">Sign Up</a> 
+                      
                     </ul>
 
 
